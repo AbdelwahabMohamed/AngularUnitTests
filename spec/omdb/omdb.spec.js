7f5d8a7ca8dc604a39446e1ae100ca963eb49c6e
Abdelwahab Sadek <ABO3@area1.eurofins.local>
2017-03-29 17:02:22 +0200

    mocking httpBackend

--- spec/omdb/omdb.spec.js
+++ spec/omdb/omdb.spec.js
@@ -1,27 +1,6 @@
 describe('omdb service', function () {
     //Test data
-    var movieData = {
-            "Title": "Star Wars: Episode IV - A New Hope",
-            "Year": "1977",
-            "Rated": "PG",
-            "Released": "25 May 1977",
-            "Runtime": "121 min",
-            "Genre": "Action, Adventure, Fantasy",
-            "Director": "George Lucas",
-            "Writer": "George Lucas",
-            "Actors": "Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing",
-            "Plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
-            "Language": "English",
-            "Country": "USA",
-            "Awards": "Won 6 Oscars. Another 50 wins & 28 nominations.",
-            "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
-            "Metascore": "92",
-            "imdbRating": "8.7",
-            "imdbVotes": "963,318",
-            "imdbID": "tt0076759",
-            "Type": "movie",
-            "Response": "True"
-        },
+    var movieData = {"Title":"Star Wars: Episode IV - A New Hope","Year":"1977","Rated":"PG","Released":"25 May 1977","Runtime":"121 min","Genre":"Action, Adventure, Fantasy","Director":"George Lucas","Writer":"George Lucas","Actors":"Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing","Plot":"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.","Language":"English","Country":"USA","Awards":"Won 6 Oscars. Another 50 wins & 28 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"8.7/10"},{"Source":"Rotten Tomatoes","Value":"93%"},{"Source":"Metacritic","Value":"92/100"}],"Metascore":"92","imdbRating":"8.7","imdbVotes":"963,318","imdbID":"tt0076759","Type":"movie","DVD":"21 Sep 2004","BoxOffice":"N/A","Production":"20th Century Fox","Website":"http://www.starwars.com/episode-iv/","Response":"True"},
         movieDataById = {
             "Title": "Star Wars: Episode IV - A New Hope",
             "Year": "1977",
@@ -44,16 +23,34 @@ describe('omdb service', function () {
             "Type": "movie",
             "Response": "True"
         },
-        omdbApi = {};
-    beforeEach(angular.mock.module('omdb'));//adding the mocked module to angular mock
-    beforeEach(angular.mock.inject(function (_omdbApi_) { //wrapper around the angular inject service, bootstraps our app and initializes all mocked modules
+        omdbApi = {},
+        $httpBackend;
+
+    //Test setup    
+    beforeEach(angular.mock.module('omdb')); //adding the mocked module to angular mock
+    beforeEach(angular.mock.inject(function (_omdbApi_, _$httpBackend_) { //wrapper around the angular inject service, bootstraps our app and initializes all mocked modules
         omdbApi = _omdbApi_;
+        $httpBackend = _$httpBackend_;
     }));
 
+    //tests
     it('should return search movie data', function () {
-        console.log(angular.mock.dump(movieData));        
-        expect(omdbApi.search('star wars')).toEqual(movieData);
-    }); 
+        //arrange
+        var response;
+        var baseUrl = 'http://www.omdbapi.com/?';
+        var searchString = 'star wars';
+        var url = baseUrl + 't=' + encodeURIComponent(searchString);
+        $httpBackend.when('GET', url).respond(200, movieData);
+        console.log(url);
+        //act
+        omdbApi.search(searchString).then(function (data) {
+            response = data;
+        });
+        $httpBackend.flush();//resolve all calls
+        
+        //assert
+        expect(response.data).toEqual(movieData);
+    });
 
     it('should return movie data by id', function () {
         expect(omdbApi.searchById('tt0076759')).toEqual(movieDataById);
--- src/omdb/omdb.data.js
+++ src/omdb/omdb.data.js
@@ -1,56 +1,56 @@
 (function () {
     'use strict';
     angular.module('omdb', []);
-    angular.module('omdb').factory('omdbApi', function () {
-        var service = {};
-        service.search = function (query) {
-            return {
-                "Title": "Star Wars: Episode IV - A New Hope",
-                "Year": "1977",
-                "Rated": "PG",
-                "Released": "25 May 1977",
-                "Runtime": "121 min",
-                "Genre": "Action, Adventure, Fantasy",
-                "Director": "George Lucas",
-                "Writer": "George Lucas",
-                "Actors": "Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing",
-                "Plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
-                "Language": "English",
-                "Country": "USA",
-                "Awards": "Won 6 Oscars. Another 50 wins & 28 nominations.",
-                "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
-                "Metascore": "92",
-                "imdbRating": "8.7",
-                "imdbVotes": "963,318",
-                "imdbID": "tt0076759",
-                "Type": "movie",
-                "Response": "True"
+    angular.module('omdb')
+        .factory('omdbApi', function ($http, $q) {
+            var service = {};
+            var baseUrl = 'http://www.omdbapi.com/?';
+
+            service.search = function (query) {
+                var deferred = $q.defer();
+                var uri = baseUrl + 't=' + encodeURIComponent(query);
+
+                $http.get(uri).then(success).catch(error).then(finallyMethod);
+
+                function success(data) {
+                    console.log('in success: ' + data);
+                    deferred.resolve(data);
+                }
+
+                function error(error) {
+                    console.log('in error');
+                }
+
+                function finallyMethod(data){
+                    console.log('in finally');
+                }
+                return deferred.promise;
             };
-        };
-        service.searchById = function (id) {
-            return {
-                "Title": "Star Wars: Episode IV - A New Hope",
-                "Year": "1977",
-                "Rated": "PG",
-                "Released": "25 May 1977",
-                "Runtime": "121 min",
-                "Genre": "Action, Adventure, Fantasy",
-                "Director": "George Lucas",
-                "Writer": "George Lucas",
-                "Actors": "Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing",
-                "Plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
-                "Language": "English",
-                "Country": "USA",
-                "Awards": "Won 6 Oscars. Another 50 wins & 28 nominations.",
-                "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
-                "Metascore": "92",
-                "imdbRating": "8.7",
-                "imdbVotes": "963,318",
-                "imdbID": "tt0076759",
-                "Type": "movie",
-                "Response": "True"
+
+            service.searchById = function (id) {
+                return {
+                    "Title": "Star Wars: Episode IV - A New Hope",
+                    "Year": "1977",
+                    "Rated": "PG",
+                    "Released": "25 May 1977",
+                    "Runtime": "121 min",
+                    "Genre": "Action, Adventure, Fantasy",
+                    "Director": "George Lucas",
+                    "Writer": "George Lucas",
+                    "Actors": "Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing",
+                    "Plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
+                    "Language": "English",
+                    "Country": "USA",
+                    "Awards": "Won 6 Oscars. Another 50 wins & 28 nominations.",
+                    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+                    "Metascore": "92",
+                    "imdbRating": "8.7",
+                    "imdbVotes": "963,318",
+                    "imdbID": "tt0076759",
+                    "Type": "movie",
+                    "Response": "True"
+                };
             };
-        };
-        return service;
-    });
+            return service;
+        });
 })();
