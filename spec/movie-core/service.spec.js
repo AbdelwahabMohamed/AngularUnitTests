4ddec57f0ff8499c7f106f7b0786cee2d77e1a6f
Abdelwahab Sadek <ABO3@area1.eurofins.local>
2017-03-30 15:30:09 +0200

    configuring and unit testing resource request headers

---
+++ spec/movie-core/service.spec.js
@@ -0,0 +1,99 @@
+describe('MovieCore', function () {
+    var PopularMovies,
+        $httpBackend;
+
+    beforeEach(module('movieCore'));
+    beforeEach(inject(function (_PopularMovies_, _$httpBackend_) {
+        PopularMovies = _PopularMovies_;
+        $httpBackend = _$httpBackend_;
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should create popular movie', function () {
+        //arrange
+        //'{"movieId":"tt0076759","describtion":"pla pla pla"}'
+        var movieId = 'tt0076759';
+        var expectedData = function (data) {
+            return angular.fromJson(data).movieId === movieId;
+        };
+        $httpBackend.expectPOST(/./, expectedData).respond(201);
+
+        var popularmovie = new PopularMovies({
+            movieId: movieId,
+            describtion: 'pla pla pla'
+        });
+        //act
+        popularmovie.$save();
+        //assert
+        expect($httpBackend.flush).not.toThrow();
+    });
+
+    it('should get popular movie by id', function () {
+        //arrange
+        var movieId = 'tt0076759';
+        //'popular/tt0076759'
+        $httpBackend.expectGET('popular/tt0076759').respond(200);
+        //act
+        PopularMovies.get({
+            movieId: movieId
+        });
+        //assert
+        expect($httpBackend.flush).not.toThrow();
+    });
+
+    it('should update popular movie', function () {
+        //arrange
+        var movieId = 'tt0076759';
+        //'{"movieId":"tt0076759","describtion":"pla pla pla updated"}'
+        var expectedData = function (data) {
+            return angular.fromJson(data).describtion === 'pla pla pla updated';
+        };
+        $httpBackend.expectPUT('popular', expectedData).respond(200);
+
+        var popularMovie = new PopularMovies({
+            movieId: movieId,
+            describtion: 'pla pla pla updated'
+        });
+
+        //act
+        popularMovie.$update();
+
+        //assert
+        expect($httpBackend.flush).not.toThrow();
+    });
+    it('should set authenticate headers correctly', function () {
+        //{"authToken":"x","Accept":"application/json, text/plain, */*"}
+        //arrange
+        var movie = {
+            movieId: 'tt0076759',
+            description: 'pla pla pla'
+        };
+        var expectedAuthToken = function (headers) {
+            return angular.fromJson(headers).authToken === 'x';
+        };
+        var matchAny = /.*/;
+
+        $httpBackend.whenGET(matchAny, expectedAuthToken).respond(200); //used when because we are getting twice
+        $httpBackend.expectPOST(matchAny, matchAny, expectedAuthToken).respond(200);
+        $httpBackend.expectPUT(matchAny, matchAny, expectedAuthToken).respond(200);
+        $httpBackend.expectDELETE(matchAny, expectedAuthToken).respond(200);
+
+        //act
+        PopularMovies.query({
+            id: movie.movieId
+        });
+        PopularMovies.get({
+            id: movie.movieId
+        });
+        new PopularMovies(movie).$save();
+        new PopularMovies(movie).$update();
+        new PopularMovies(movie).$remove();
+
+        //assert
+        expect($httpBackend.flush).not.toThrow();
+    });
+});
--- src/movie-core/service.js
+++ src/movie-core/service.js
@@ -2,11 +2,39 @@
     'use strict';
     angular.module('movieCore', ['ngResource'])
         .factory('PopularMovies', function ($resource) {
+            var token = 'x';
             return $resource('popular/:movieId', {
                 movieId: '@id'
             }, {
                 update: {
-                    method: 'PUT'
+                    method: 'PUT',
+                    headers: {
+                        'authToken': token
+                    }
+                },
+                get: {
+                    method: 'GET',
+                    headers: {
+                        'authToken': token
+                    }
+                },
+                query: {
+                    method: 'GET',
+                    headers: {
+                        'authToken': token
+                    }
+                },
+                save: {
+                    method: 'POST',
+                    headers: {
+                        'authToken': token
+                    }
+                },
+                remove: {
+                    method: 'DELETE',
+                    headers: {
+                        'authToken': token
+                    }
                 }
             });
         });
