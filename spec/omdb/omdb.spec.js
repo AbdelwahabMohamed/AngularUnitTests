b90306a623fe000e0e5a26a930d3f9a1f5bbbddb
Abdelwahab Sadek <ABO3@area1.eurofins.local>
2017-03-30 09:26:29 +0200

    Using mocked httpBackend object

--- spec/omdb/omdb.spec.js
+++ spec/omdb/omdb.spec.js
@@ -1,6 +1,41 @@
 describe('omdb service', function () {
     //Test data
-    var movieData = {"Title":"Star Wars: Episode IV - A New Hope","Year":"1977","Rated":"PG","Released":"25 May 1977","Runtime":"121 min","Genre":"Action, Adventure, Fantasy","Director":"George Lucas","Writer":"George Lucas","Actors":"Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing","Plot":"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.","Language":"English","Country":"USA","Awards":"Won 6 Oscars. Another 50 wins & 28 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"8.7/10"},{"Source":"Rotten Tomatoes","Value":"93%"},{"Source":"Metacritic","Value":"92/100"}],"Metascore":"92","imdbRating":"8.7","imdbVotes":"963,318","imdbID":"tt0076759","Type":"movie","DVD":"21 Sep 2004","BoxOffice":"N/A","Production":"20th Century Fox","Website":"http://www.starwars.com/episode-iv/","Response":"True"},
+    var movieData = {
+            "Title": "Star Wars: Episode IV - A New Hope",
+            "Year": "1977",
+            "Rated": "PG",
+            "Released": "25 May 1977",
+            "Runtime": "121 min",
+            "Genre": "Action, Adventure, Fantasy",
+            "Director": "George Lucas",
+            "Writer": "George Lucas",
+            "Actors": "Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing",
+            "Plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
+            "Language": "English",
+            "Country": "USA",
+            "Awards": "Won 6 Oscars. Another 50 wins & 28 nominations.",
+            "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+            "Ratings": [{
+                "Source": "Internet Movie Database",
+                "Value": "8.7/10"
+            }, {
+                "Source": "Rotten Tomatoes",
+                "Value": "93%"
+            }, {
+                "Source": "Metacritic",
+                "Value": "92/100"
+            }],
+            "Metascore": "92",
+            "imdbRating": "8.7",
+            "imdbVotes": "963,318",
+            "imdbID": "tt0076759",
+            "Type": "movie",
+            "DVD": "21 Sep 2004",
+            "BoxOffice": "N/A",
+            "Production": "20th Century Fox",
+            "Website": "http://www.starwars.com/episode-iv/",
+            "Response": "True"
+        },
         movieDataById = {
             "Title": "Star Wars: Episode IV - A New Hope",
             "Year": "1977",
@@ -24,6 +59,7 @@ describe('omdb service', function () {
             "Response": "True"
         },
         omdbApi = {},
+        baseUrl = 'http://www.omdbapi.com/?',
         $httpBackend;
 
     //Test setup    
@@ -36,42 +72,34 @@ describe('omdb service', function () {
     //tests
     it('should return search movie data', function () {
         //arrange
-        var response;
-        var baseUrl = 'http://www.omdbapi.com/?';
-        var searchString = 'star wars';
-        var url = baseUrl + 't=' + encodeURIComponent(searchString);
+        var response = null,
+            searchString = 'star wars',
+            url = baseUrl + 't=' + encodeURIComponent(searchString);
         $httpBackend.when('GET', url).respond(200, movieData);
-        console.log(url);
+
         //act
         omdbApi.search(searchString).then(function (data) {
             response = data;
         });
-        $httpBackend.flush();//resolve all calls
-        
+        $httpBackend.flush(); //resolve all calls
+
         //assert
         expect(response.data).toEqual(movieData);
     });
 
     it('should return movie data by id', function () {
-        expect(omdbApi.searchById('tt0076759')).toEqual(movieDataById);
+        //arrange
+        var response,
+            id = 'tt0076759',
+            url = baseUrl + 'i=' + encodeURIComponent(id);
+            console.log(url);
+        $httpBackend.when('GET', url).respond(200, movieDataById);
+        //act
+        omdbApi.searchById(id).then(function (data) {
+            response = data;
+        });
+        $httpBackend.flush();
+        //assert
+        expect(response.data).toEqual(movieDataById);
     });
 });
-/**other ways to use the mock module functionality */
-//this way, we can`t send anything else other than the $provide that is being sent implicityly, use for brainstorming
-// angular.mock.module({
-//     'omdbApi': {
-//         search: function (query) {
-//             return movieData;
-//         }
-//     }
-// });
-//another way to configure the module, also creates an inline implementation, use for brainstorming
-// angular.mock.module(function ($provide) {
-//     $provide.factory('omdbApi', function () {
-//         return {
-//             search: function (query) {
-//                 return movieData;
-//             } 
-//         };
-//     });
-// });
