fdac15c75a42f066f045bce98ef949330e840af6
Abdelwahab Sadek <ABO3@area1.eurofins.local>
2017-04-11 14:34:27 +0200

    using main-bower-files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
+var bowerFiles = require('main-bower-files');
 var $ = require('gulp-load-plugins')({
     lazy: true
 });
 
 gulp.task('inject', function () {
     return gulp.src('src/index.html')
-        .pipe($.inject(gulp.src(['./src/**/*.module.js','./src/**/*.js','./lib/**/**/*.js'])))
+        .pipe($.inject(gulp.src(bowerFiles(), {
+            read: false
+        }), {
+            name: 'bower'
+        }))
+        .pipe($.inject(gulp.src(['./src/**/*.module.js', './src/**/*.js', './lib/**/**/jasmine.js', './lib/**/**/jasmine-*.js','./lib/**/**/*.js'], {read: false}), {relative: true}))
         .pipe(gulp.dest('src'));
 });
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/src'));
+app.use( express.static(__dirname + '/src'));
 //add this so the browser can GET the bower files
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/lib', express.static(__dirname + '/lib'));
